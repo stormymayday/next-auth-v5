2d02898baa32ed4cfceb0a38ee55073c36f4b278
stormymayday <stormymayday7@gmail.com>
2024-12-07 14:22:48 -0800

    Home page and LoginButton component

---
+++ components/auth/LoginButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface LoginButtonProps {
+    children: React.ReactNode;
+    mode?: "modal" | "redirect";
+    asChild?: boolean;
+}
+
+function LoginButton({
+    children,
+    mode = "redirect",
+}: // asChild,
+LoginButtonProps) {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push("/auth/login");
+    };
+
+    if (mode === "modal") {
+        return <span>TODO: Implement modal</span>;
+    }
+
+    return (
+        <span onClick={handleClick} className="cursor-pointer">
+            {children}
+        </span>
+    );
+}
+export default LoginButton;
