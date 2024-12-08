811aa00ce8ab9054a4feb611ce3e8550b1d2e5eb
stormymayday <stormymayday7@gmail.com>
2024-12-08 13:29:27 -0800

    extending session to add id and role

--- auth.ts
+++ auth.ts
@@ -1,7 +1,29 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
+import { getUserById } from "@/utils/getUserById";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { JWT } from "next-auth/jwt";
+import { UserRole } from "@prisma/client";
+
+type ExtendedUser = DefaultSession["user"] & {
+    role: UserRole;
+    // add extra fields here:
+    // newCustomField: string;
+};
+
+declare module "next-auth" {
+    interface Session {
+        user: ExtendedUser;
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        role?: "ADMIN" | "USER";
+    }
+}
 
 export const {
     handlers: { GET, POST },
@@ -9,6 +31,38 @@ export const {
     signIn,
     signOut,
 } = NextAuth({
+    callbacks: {
+        async session({ token, session }) {
+            console.log({
+                sessionToken: token,
+            });
+
+            if (token.role && session.user) {
+                session.user.role = token.role;
+            }
+
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+            }
+
+            return session;
+        },
+        async jwt({ token }) {
+            if (!token.sub) {
+                return token;
+            }
+
+            const existingUser = await getUserById(token.sub);
+
+            if (!existingUser) {
+                return token;
+            }
+
+            token.role = existingUser.role;
+
+            return token;
+        },
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
     ...authConfig,
