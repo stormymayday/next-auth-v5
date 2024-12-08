93cf534511a75ec283e4cfd853d854bbb2da951a
stormymayday <stormymayday7@gmail.com>
2024-12-07 23:27:53 -0800

    removed type assertion

--- utils/getUserByEmail.ts
+++ utils/getUserByEmail.ts
@@ -1,12 +1,11 @@
 import { db } from "@/lib/db";
-import { Prisma } from "@prisma/client";
 
 export async function getUserByEmail(email: string) {
     try {
         const user = await db.user.findUnique({
             where: {
                 email: email,
-            } as Prisma.UserWhereUniqueInput,
+            },
         });
 
         return user;
