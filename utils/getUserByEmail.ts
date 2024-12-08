d11b205a506c3edb0bf837884dc35193cf4469fa
stormymayday <stormymayday7@gmail.com>
2024-12-07 23:25:53 -0800

    Explicitly specifying email field and added type assertion

--- utils/getUserByEmail.ts
+++ utils/getUserByEmail.ts
@@ -1,11 +1,12 @@
 import { db } from "@/lib/db";
+import { Prisma } from "@prisma/client";
 
 export async function getUserByEmail(email: string) {
     try {
         const user = await db.user.findUnique({
             where: {
-                email,
-            },
+                email: email,
+            } as Prisma.UserWhereUniqueInput,
         });
 
         return user;
