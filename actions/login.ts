63c96c8aa1e281d226079aee5167709025167a46
stormymayday <stormymayday7@gmail.com>
2024-12-08 10:49:56 -0800

    Login

--- actions/login.ts
+++ actions/login.ts
@@ -2,6 +2,10 @@
 
 import * as z from "zod";
 import { LoginSchema } from "@/schemas";
+// import { getUserByEmail } from "@/utils/getUserByEmail";
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { AuthError } from "next-auth";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
@@ -12,5 +16,31 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
         };
     }
 
-    return { success: "Email sent!" };
+    const { email, password } = validatedFields.data;
+
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT,
+        });
+
+        return {
+            success: "Login successful!",
+        };
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return {
+                        error: "Invalid credentials!",
+                    };
+                default:
+                    return {
+                        error: "Something went wrong!",
+                    };
+            }
+        }
+        throw error;
+    }
 };
--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,39 @@
-import GitHub from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "@/utils/getUserByEmail";
+import bcrypt from "bcryptjs";
 
 import type { NextAuthConfig } from "next-auth";
 
 export default {
-    providers: [GitHub],
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const validatedFields = LoginSchema.safeParse(credentials);
+
+                if (!validatedFields.success) {
+                    return null;
+                }
+
+                const { email, password } = validatedFields.data;
+
+                const user = await getUserByEmail(email);
+
+                if (!user || !user.password) {
+                    return null;
+                }
+
+                const passwordMatch = await bcrypt.compare(
+                    password,
+                    user.password
+                );
+
+                if (!passwordMatch) {
+                    return null;
+                }
+
+                return user;
+            },
+        }),
+    ],
 } satisfies NextAuthConfig;
