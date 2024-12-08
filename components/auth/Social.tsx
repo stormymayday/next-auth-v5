30b2b68d50040478063fd49d1226622b133b848c
stormymayday <stormymayday7@gmail.com>
2024-12-08 14:24:24 -0800

    Google and GitHub signIn

--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -3,15 +3,22 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/shadcn-ui/Button";
+import { signIn } from "next-auth/react";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 function Social() {
+    const onClick = (provider: "google" | "github") => {
+        signIn(provider, {
+            callbackUrl: DEFAULT_LOGIN_REDIRECT,
+        });
+    };
     return (
         <div className="w-full flex gap-x-2 items-center justify-center">
             <Button
                 variant="outline"
                 size="lg"
                 className="w-full"
-                onClick={() => {}}
+                onClick={() => onClick("google")}
             >
                 <FcGoogle />
             </Button>
@@ -19,7 +26,7 @@ function Social() {
                 variant="outline"
                 size="lg"
                 className="w-full"
-                onClick={() => {}}
+                onClick={() => onClick("github")}
             >
                 <FaGithub />
             </Button>
