e852eef0eef153f16a726395411750e7fafc87f8
stormymayday <stormymayday7@gmail.com>
2024-12-08 10:55:01 -0800

    signOut

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,8 +1,20 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 async function SettingsPage() {
     const session = await auth();
 
-    return <div>{JSON.stringify(session)}</div>;
+    return (
+        <div>
+            {JSON.stringify(session)}
+            <form
+                action={async () => {
+                    "use server";
+                    await signOut();
+                }}
+            >
+                <button type="submit">sign out</button>
+            </form>
+        </div>
+    );
 }
 export default SettingsPage;
