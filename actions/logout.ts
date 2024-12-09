2e4f4372b61c1fd291f8a5905fab19150ee235ca
stormymayday <stormymayday7@gmail.com>
2024-12-09 08:09:15 -0800

    Testing settings page as a client component

---
+++ actions/logout.ts
@@ -0,0 +1,8 @@
+"use server";
+
+import { signOut } from "@/auth";
+
+export const logout = async () => {
+    // Do some server stuff before logging out
+    await signOut({ redirectTo: "/auth/login" });
+};
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,20 +1,46 @@
-import { auth, signOut } from "@/auth";
+"use client";
 
-async function SettingsPage() {
-    const session = await auth();
+import { logout } from "@/actions/logout";
+import { useSession } from "next-auth/react";
+
+function SettingsPage() {
+    // extracting the session data.
+    const { data: session } = useSession();
+
+    // console.log(session);
+
+    const onClick = () => {
+        logout();
+    };
 
     return (
         <div>
             {JSON.stringify(session)}
-            <form
-                action={async () => {
-                    "use server";
-                    await signOut({ redirectTo: "/auth/login" });
-                }}
-            >
-                <button type="submit">sign out</button>
-            </form>
+            <button onClick={onClick}>sign out</button>
         </div>
     );
 }
 export default SettingsPage;
+
+// import { auth, signOut } from "@/auth";
+// import { logout } from "@/actions/logout";
+
+// async function SettingsPage() {
+//     const session = await auth();
+
+//     return (
+//         <div>
+//             {JSON.stringify(session)}
+//             <form
+//                 action={async () => {
+//                     "use server";
+//                     // await signOut({ redirectTo: "/auth/login" });
+//                     await logout();
+//                 }}
+//             >
+//                 <button type="submit">sign out</button>
+//             </form>
+//         </div>
+//     );
+// }
+// export default SettingsPage;
