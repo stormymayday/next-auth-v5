6e6b04ee80896c532daa972eb27c121597cd59a6
stormymayday <stormymayday7@gmail.com>
2024-12-10 13:24:58 -0800

    Navbar responsivness

--- app/(protected)/_components/Navbar.tsx
+++ app/(protected)/_components/Navbar.tsx
@@ -9,7 +9,7 @@ function Navbar() {
     const pathname = usePathname();
 
     return (
-        <nav className="w-[95vw] max-w-[600px] mt-5 bg-slate-50 h-16 flex justify-between items-center p-4 rounded-sm shadow-sm">
+        <nav className="w-full max-w-[600px] mt-5 bg-slate-50 h-auto min-h-16 flex flex-col sm:flex-row justify-between items-center p-4 rounded-sm shadow-sm space-y-4 sm:space-y-0">
             <div className="flex gap-x-2">
                 <Button
                     variant={pathname === "/server" ? "default" : "outline"}
